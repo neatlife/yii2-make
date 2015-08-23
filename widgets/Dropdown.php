a1f5c12186f941b6cc75d4c4b0599f1d83ad5294
suxiaolin <suxiaolin@mail.com>
2015-08-23 17:57:46 +0800

    finish nav integrate

---
+++ widgets/Dropdown.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace suxiaolin\make\widgets;
+
+use yii\bootstrap\Widget;
+use yii\base\InvalidConfigException;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+class Dropdown extends \yii\bootstrap\Dropdown {
+    public $options = ['class' => 'children collapse'];
+    
+    public function init()
+    {
+        Widget::init();
+    }
+    
+    protected function renderItems($items, $options = [])
+    {
+        $lines = [];
+        foreach ($items as $i => $item) {
+            if (isset($item['visible']) && !$item['visible']) {
+                continue;
+            }
+            if (is_string($item)) {
+                $lines[] = $item;
+                continue;
+            }
+            if (!array_key_exists('label', $item)) {
+                throw new InvalidConfigException("The 'label' option is required.");
+            }
+            $encodeLabel = isset($item['encode']) ? $item['encode'] : $this->encodeLabels;
+            $label = $encodeLabel ? Html::encode($item['label']) : $item['label'];
+            $itemOptions = ArrayHelper::getValue($item, 'options', []);
+            $linkOptions = ArrayHelper::getValue($item, 'linkOptions', []);
+            $linkOptions['tabindex'] = '-1';
+            $url = array_key_exists('url', $item) ? $item['url'] : null;
+            if (empty($item['items'])) {
+                if ($url === null) {
+                    $content = $label;
+                } else {
+                    $content = Html::a($label, $url, $linkOptions);
+                }
+            } else {
+                $submenuOptions = $options;
+                unset($submenuOptions['id']);
+                $content = Html::a($label, $url === null ? '#' : $url, $linkOptions)
+                    . $this->renderItems($item['items'], $submenuOptions);
+            }
+
+            $lines[] = Html::tag('li', $content, $itemOptions);
+        }
+
+        return Html::tag('ul', implode("\n", $lines), $options);
+    }
+}
---
+++ widgets/Nav.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace suxiaolin\make\widgets;
+
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+
+class Nav extends \yii\bootstrap\Nav {
+    public $dropDownCaret = '<span class="fa arrow"></span>';
+    
+    public function init() {
+        parent::init();
+        Html::addCssClass($this->options, 'nav-sidebar');
+    }
+    
+    public function renderItem($item)
+    {
+        if (is_string($item)) {
+            return $item;
+        }
+        if (!isset($item['label'])) {
+            throw new InvalidConfigException("The 'label' option is required.");
+        }
+        $encodeLabel = isset($item['encode']) ? $item['encode'] : $this->encodeLabels;
+        $label = Html::tag('span', $encodeLabel ? Html::encode($item['label']) : $item['label']);
+        $icon = isset($item['iconClass']) ? Html::tag('i', '', ['class' => $item['iconClass']]) : '';
+        $label = $icon . $label;
+        $options = ArrayHelper::getValue($item, 'options', []);
+        $items = ArrayHelper::getValue($item, 'items');
+        $url = ArrayHelper::getValue($item, 'url', '#');
+        $linkOptions = ArrayHelper::getValue($item, 'linkOptions', []);
+
+        if (isset($item['active'])) {
+            $active = ArrayHelper::remove($item, 'active', false);
+        } else {
+            $active = $this->isItemActive($item);
+        }
+
+        if ($items !== null) {
+            Html::addCssClass($options, 'nav-parent');
+            if ($this->dropDownCaret !== '') {
+                $label .= ' ' . $this->dropDownCaret;
+            }
+            if (is_array($items)) {
+                if ($this->activateItems) {
+                    $items = $this->isChildActive($items, $active);
+                }
+                $items = $this->renderDropdown($items, $item);
+            }
+        }
+
+        if ($this->activateItems && $active) {
+            Html::addCssClass($options, 'active');
+        }
+
+        return Html::tag('li', Html::a($label, $url, $linkOptions) . $items, $options);
+    }
+    
+    protected function renderDropdown($items, $parentItem)
+    {
+        return Dropdown::widget([
+            'items' => $items,
+            'encodeLabels' => $this->encodeLabels,
+            'clientOptions' => false,
+            'view' => $this->getView(),
+        ]);
+    }
+}
